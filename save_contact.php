edb9daed40e1fba3421d3973d8a18f0d0bee87e6
msreddyfrnd <msreddy.frnd@gmail.com>
2026-05-16 11:35:55 +0530

    Delete save_contact.php

--- save_contact.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'config.php';
-$name=$_POST['name'];
-$email=$_POST['email'];
-$message=$_POST['message'];
-mysqli_query($conn,"INSERT INTO contacts(name,email,message) VALUES('$name','$email','$message')");
-echo 'Saved';
-?>
